1599d42df37b762fa4db23ab70b7df69c489618b
Oliver Schafeld <info@schafeld.com>
2015-02-18 09:58:59 +0000

    added shader

--- script.js
+++ script.js
@@ -5,4 +5,32 @@ var main=function() {
   CANVAS.width=window.innerWidth;
   CANVAS.height=window.innerHeight;
 
+  /*========================= GET WEBGL CONTEXT ========================= */
+  var GL;
+  try {
+    GL = CANVAS.getContext("experimental-webgl", {antialias: false});
+  } catch (e) {
+    alert("You are not webgl compatible : ") ;
+    return false;
+  }
+
+  /*========================= SHADERS ========================= */
+  /*jshint multistr: true */
+  var shader_vertex_source="\n\
+attribute vec2 position; //the position of the point\n\
+void main(void) { //pre-built function\n\
+gl_Position = vec4(position, 0., 1.); //0. is the z, and 1 is w\n\
+}";
+
+
+  var shader_fragment_source="\n\
+precision mediump float;\n\
+\n\
+\n\
+\n\
+void main(void) {\n\
+gl_FragColor = vec4(0.,0.,0., 1.); //black color\n\
+}";
+
+  
 };
